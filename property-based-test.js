6666eb314c47c5693c1556c7521c16a448da8e75
Alexey Y <alexey.yashenkov@gmail.com>
2022-06-21 16:23:12 +0300

    Update property-based-test.js

--- property-based-test.js
+++ property-based-test.js
@@ -22,3 +22,37 @@ test('string should always contain its substring', () => {
     )
   );
 });
+
+
+// @ts-check
+
+import fc from 'fast-check';
+import getFunction from '../functions.js';
+
+const sort = getFunction();
+
+// BEGIN (write your solution here)
+test('prop based test', () => {
+  fc.assert(
+    fc.property(fc.array(fc.integer()), (data) => {
+      const sorted = sort(data);
+      expect(sorted.length).toEqual(data.length);
+      expect(sorted).toBeSorted();
+    })
+  );
+})
+// END
+
+
+// @ts-check
+
+const functions = {
+  right1: (data) => data.slice().sort((a, b) => a - b),
+  wrong1: (data) => data.slice().sort((a, b) => b - a),
+  wrong2: (data) => data.slice().reverse(),
+};
+
+export default () => {
+  const name = process.env.FUNCTION_VERSION || 'right1';
+  return functions[name];
+};
