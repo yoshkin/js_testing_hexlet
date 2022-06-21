da40ad9e236a9a41e70b320bbc885f0403405747
Alexey Y <alexey.yashenkov@gmail.com>
2022-06-21 15:33:56 +0300

    Update getUserMainLanguage.test.js

--- getUserMainLanguage.test.js
+++ getUserMainLanguage.test.js
@@ -13,6 +13,42 @@
 // support/OctokitFake.js
 // Реализуйте фейковый клиент по такому же принципу как это было сделано в теории. Используйте этот клиент в тестах для подмены.
 
+
+
+/////// v2
+
+// @ts-check
+
+import nock from 'nock';
+import getFunction from '../functions.js';
+
+const getUserMainLanguage = getFunction();
+
+nock.disableNetConnect();
+
+test('getUserMainLanguage', async () => {
+  const data = [
+    { language: 'ruby' },
+    { language: 'php' },
+    { language: 'java' },
+    { language: 'php' },
+    { language: 'js' },
+  ];
+  nock(/api\.github\.com/).get('/users/linus/repos').reply(200, data);
+
+  const mainLanguage = await getUserMainLanguage('linus');
+  expect(mainLanguage).toEqual('php');
+});
+
+test('getUserMainLanguage when empty', async () => {
+  nock(/api\.github\.com/).get('/users/user-without-repos/repos').reply(200, []);
+
+  const mainLanguage = await getUserMainLanguage('user-without-repos');
+  expect(mainLanguage).toBeNull();
+});
+
+/////// v2
+
 // @ts-check
 
 import OctokitFake from '../support/OctokitFake.js';
